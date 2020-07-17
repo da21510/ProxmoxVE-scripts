19b855bf535aebd9f21c26d5cd09e5ce25a01117
da21510 <da21510@gmail.com>
2020-07-18 06:56:16 +0800

    first commit

---
+++ PVE_init.sh
@@ -0,0 +1,13 @@
+echo "" >> /etc/apt/sources.list
+echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" >> /etc/apt/sources.list
+sed -i 's/deb/#&/g' /etc/apt/sources.list.d/pve-enterprise.list
+apt update
+apt upgrade -y
+
+apt install -y vim
+
+sed -i 's/if (data.status !== '"'"'Active'"'"') {/if (false) {/g' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+
+wget -qO- https://raw.githubusercontent.com/da21510/gpupassthrough_script/master/setup-modules.sh | bash
+wget -qO- https://raw.githubusercontent.com/da21510/gpupassthrough_script/master/setup-vfio.sh | bash
+update-initramfs -u -k all
